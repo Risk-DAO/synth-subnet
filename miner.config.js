8ce2fa9f3b21bd8a682d8cfc936becccb011350b
yaronvel <yaron172@yahoo.com>
2025-08-28 22:00:55 +0300

    Update miner.config.js

--- miner.config.js
+++ miner.config.js
@@ -26,6 +26,15 @@ module.exports = {
       env: {
         PYTHONPATH: ".",
       },
+    },
+    {
+      name: "miner4",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_4 --axon.port 8093 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
     },    
   ],
 };
