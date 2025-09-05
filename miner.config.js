6159310d942c29ab91b0b76948f36179c4989fd3
yaronvel <yaron172@yahoo.com>
2025-09-05 13:07:40 +0300

    Update miner.config.js

--- miner.config.js
+++ miner.config.js
@@ -63,7 +63,7 @@ module.exports = {
         PYTHONPATH: ".",
       },
     },
-    {
+{
       name: "miner8",
       interpreter: "python3",
       script: "./neurons/miner.py",
@@ -71,6 +71,114 @@ module.exports = {
       env: {
         PYTHONPATH: ".",
       },
-    },      
+    },
+    {
+      name: "miner9",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_9 --axon.port 8099 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner10",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_10 --axon.port 8100 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner11",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_11 --axon.port 8101 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner12",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_12 --axon.port 8102 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },    
+    {
+      name: "miner13",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_13 --axon.port 8103 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner14",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_14 --axon.port 8104 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner15",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_15 --axon.port 8105 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner16",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_16 --axon.port 8106 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner17",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_17 --axon.port 8107 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner18",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_18 --axon.port 8108 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner19",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_19 --axon.port 8109 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "miner20",
+      interpreter: "python3",
+      script: "./neurons/miner.py",
+      args: "--netuid 50 --logging.debug --logging.trace --wallet.name miner --wallet.hotkey miner_20 --axon.port 8110 --blacklist.force_validator_permit true --blacklist.validator_min_stake 1",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },    
   ],
 };
