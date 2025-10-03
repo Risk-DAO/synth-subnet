22aa5e3e5a7e76e630f22702135f054fcc93cf80
yaronvel <yaron172@yahoo.com>
2025-10-03 10:47:42 +0300

    Update simulations.py

--- synth/miner/simulations.py
+++ synth/miner/simulations.py
@@ -104,8 +104,7 @@ def generate_simulations(
     default_sigma = sigma = 0.003
     sqrt24 = math.sqrt(24)
     sigma = float(xxx_json["simple_avg_vol"]) / sqrt24
-
-    historical_miners = ["5D2D5rRPZSzqvS1d7KDqSzbqbL6Eo6PteShd7i2oNcXboYpi", "5HoviLrfGLJC1N2dKhuqxb33PP57v5tSg9zNd59xFZoCdymY", "5Fvfszm7RqYpqUqS9UFimsxcS5EpoD7z1D8PnwmJcYDSrzKf"]
+    historical_miners = ["5DbxBFeEfxnsNYnx95YqDG74fpxMLQQwBnGrskDCNzjJFvxW", "5GHeboa3d4QTdnboR1oQLeQiZudX9cKYyBpY6VHjxsSvCC29", "5D2D5rRPZSzqvS1d7KDqSzbqbL6Eo6PteShd7i2oNcXboYpi", "5HoviLrfGLJC1N2dKhuqxb33PP57v5tSg9zNd59xFZoCdymY", "5Fvfszm7RqYpqUqS9UFimsxcS5EpoD7z1D8PnwmJcYDSrzKf"]
     if hot_key in historical_miners:
         sigma = float(xxx_json["historical_vol"]) / sqrt24        
 
